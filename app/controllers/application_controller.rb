1d36a233fa6aba8cb4067f9214e28969887a8796
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-31 13:11:24 +0000

    working on rand

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,20 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
 
-    @num = params.fetch("elephant").to_f
-    @square_of_num = @num ** 2
+    @num = params.fetch("elephant")
+    @square_of_num = @num.to_f ** 2
 
     render({ :template => "calculation_templates/square_results_form.html.erb"})
   end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get("/square/results", { :controller => "application", :action => "calculate_square" })
 
+  get("/random/results", { :controller => "application", :action => "calculate_random"})
+
+
 
 end
